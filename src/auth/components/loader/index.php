f3c71a60870afe3ab3c4d7d98453891fad05aa7f
ejoenel16 <168512386+ejoenel16@users.noreply.github.com>
2024-07-26 07:00:34 +0800

    Delete src/auth/components directory

--- src/auth/components/loader/index.php
+++
@@ -1,7 +0,0 @@
-<div class=" justify-content-center flex-column mx-auto" style="width:300px; margin-top:6em; display:flex" id="loading_overlay">
-        <div class="bouncing-loader mt-0">
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    </div>
